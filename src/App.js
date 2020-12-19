3868c32e28df189e51a7fe9f3d76d6795d1cb32d
fauziah09 <48745584+fauziah09@users.noreply.github.com>
2020-12-19 20:52:38 +0700

    slicing component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
 
 function App() {
-
-  let a = 4;
-  let b = 6;
   return (
-    // must have parent element
-    <div className="App">
-      {a + b / 5}
+    <div className="Parentbox">
+    </div>
+  );
+}
 
+function PhotoProduct() {
+  return (
+    <div className="Photo">
+      <img src="sneakers.jpg" alt="img"></img>
+    </div>
+  );
+}
+
+function ProductInfo() {
+  const category = "SNEAKERS"
+  return (
+    <div>
+      <div className="Deskripsi">
+        <p className="Cate">{category}</p>
+        <h1 className="Title">Pinky Swag</h1>
+        <p className="Price">IDR 7.000.300</p>
+        <p className="Info">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nisi quas necessitatibus omnis error quis exercitationem ipsum deleniti reiciendis illo!
+      </p>
+      </div>
     </div>
   );
 }
