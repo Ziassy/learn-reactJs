afbc3ba9d07b7afbde9f8428950300ef3659db2f
fauziah09 <48745584+fauziah09@users.noreply.github.com>
2020-12-19 21:21:43 +0700

    Add Event

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="Parentbox">
       <PhotoProduct />
-      <ProductInfo />
+      <ProductInfo name="Pinky Swag" category="SNEAKERS" />
     </div>
   );
 }
@@ -17,20 +17,25 @@ function PhotoProduct() {
   );
 }
 
-function ProductInfo() {
-  const category = "SNEAKERS"
+function ProductInfo(props) {
+  const { name, category } = props;
   return (
     <div>
       <div className="Deskripsi">
         <p className="Cate">{category}</p>
-        <h1 className="Title">Pinky Swag</h1>
+        <h1 className="Title">{name}</h1>
         <p className="Price">IDR 7.000.300</p>
         <p className="Info">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nisi quas necessitatibus omnis error quis exercitationem ipsum deleniti reiciendis illo!
       </p>
+        <a onClick={AddCart} hre="#">Add to cart</a>
       </div>
     </div>
   );
+}
+
+function AddCart() {
+  console.log("The product has been added to cart..")
 }
 
 export default App;
