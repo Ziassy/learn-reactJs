16ed4493cb25227c6e8ccd878bf65d04d7f3c370
fauziah09 <48745584+fauziah09@users.noreply.github.com>
2020-12-25 23:28:39 +0700

    Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="Parentbox">
       <PhotoProduct />
-      <ProductInfo name="Pinky Swag" category="SNEAKERS" />
+      <ProductInfo isDiscount="yes" name="Pinky Swag" category="SNEAKERS" />
     </div>
   );
 }
@@ -17,17 +17,38 @@ function PhotoProduct() {
   );
 }
 
+function CheckDiscount(props) {
+  const { isDiscount } = props;
+  if (isDiscount === "yes") {
+    return (
+      <p>Diskon 50% off</p>
+    );
+  }
+  else if (isDiscount === "coming") {
+    return (
+      <p>Akan ada dikson ... </p>
+    );
+  }
+  else {
+    return (
+      <p>Tidak ada diskon!</p>
+    )
+  }
+}
+
 function ProductInfo(props) {
-  const { name, category } = props;
+  const { name, category, isDiscount } = props;
   return (
     <div>
       <div className="Deskripsi">
         <p className="Cate">{category}</p>
         <h1 className="Title">{name}</h1>
         <p className="Price">IDR 7.000.300</p>
+        <CheckDiscount isDiscount={isDiscount} />
         <p className="Info">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nisi quas necessitatibus omnis error quis exercitationem ipsum deleniti reiciendis illo!
       </p>
+        {/* passing arguments to event handlers */}
         <a onClick={(e) => AddCart(name, e)} hre="#">Add to cart</a>
       </div>
     </div>
@@ -35,7 +56,7 @@ function ProductInfo(props) {
 }
 
 function AddCart(e) {
-  console.log(e + "has been added to cart..")
+  alert(e + "has been added to cart..")
 }
 
 export default App;
